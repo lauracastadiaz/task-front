ee63b271e4f2afe0035f9dc4b6ff5ef76a892092
Laura Castaño <lauracastadiaz@gmail.com>
2025-01-22 20:50:15 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://restapitaskexpress-production.up.railway.app/api/tasks',  // La URL de tu backend
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),  // Opcional: reescribe la URL si es necesario
+      },
+    },
+  },
+});
