189aedeac1a27e3f1de8aa024eabcf40264e019d
Laura Castaño <lauracastadiaz@gmail.com>
2025-01-23 19:19:18 +0100

    Modificacion package json

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,4 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://restapitaskexpress-production.up.railway.app/api/tasks',  // La URL de tu backend
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),  // Opcional: reescribe la URL si es necesario
-      },
-    },
-  },
 });
