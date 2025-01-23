3cdd3a9b3bed36b9882b91e491cf61c4844a0a85
Laura Castaño <lauracastadiaz@gmail.com>
2025-01-23 21:44:08 +0100

    Update Listado.jsx

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -13,11 +13,11 @@ export const Listado = ({tasks, deleteTask, setTaskToEdit, toggleTaskCompleted }
       <div className="overflow-x-auto max-w-full">
       <Table className="table-auto border border-solid border-indigo-200 p-5 rounded-lg space-y-10 bg-slate-200 w-full">
         <TableHead>
-          <TableHeadCell className="text-xs sm:text-base">ID</TableHeadCell>
-          <TableHeadCell className="text-xs sm:text-base">Título</TableHeadCell>
-          <TableHeadCell className="text-xs sm:text-base">Descripción</TableHeadCell>
-          <TableHeadCell className="text-xs sm:text-base">Estado</TableHeadCell>
-          <TableHeadCell className="text-xs sm:text-base">
+        <TableHeadCell>ID</TableHeadCell>
+          <TableHeadCell>Título</TableHeadCell>
+          <TableHeadCell>Descripción</TableHeadCell>
+          <TableHeadCell>Estado</TableHeadCell>
+          <TableHeadCell>
             <span className="sr-only">Editar</span>
           </TableHeadCell>
           <TableHeadCell className="text-xs sm:text-base">
@@ -43,49 +43,28 @@ export const Listado = ({tasks, deleteTask, setTaskToEdit, toggleTaskCompleted }
             </TableCell>
             <TableCell className={`${
                     task.completed ? "text-gray-500 line-through" : ""
-                  }hidden sm:table-cell`}>{task.description}</TableCell>
+                  }`}>{task.description}</TableCell>
             <TableCell>
               {/* Checkbox para cambiar el estado de completado */}
               <input className="cursor-pointer" type="checkbox" checked={task.completed}  
               onChange={() => toggleTaskCompleted(task.id, !task.completed)} />
             </TableCell>
-            {/* Botones Editar y Eliminar en pantallas grandes */}
-            <TableCell className="hidden sm:table-cell">
+            <TableCell>
               <button className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
               onClick={() => setTaskToEdit(task)}
               >
                 Editar
               </button>
             </TableCell>
-            <TableCell className="hidden sm:table-cell">
-              <button className="font-medium text-red-900 hover:underline dark:text-red-300"
+            <TableCell>
+              <button  className="font-medium text-red-900 hover:underline dark:text-red-300"
               onClick={() => {
-                if (window.confirm("¿Estás seguro de que deseas eliminar esta tarea?")) {
                   deleteTask(task.id);
-                }
+                
               }}>
                 Eliminar
               </button>
             </TableCell>
-
-              {/* Botones Editar y Eliminar en pantallas pequeñas (se reorganiza en una sola celda) */}
-              <TableCell className="sm:hidden w-full flex flex-col space-y-2 items-center">
-                  <button className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
-                    onClick={() => setTaskToEdit(task)}>
-                    Editar
-                  </button>
-                  <button className="font-medium text-red-900 hover:underline dark:text-red-300"
-                    onClick={() => {
-                      if (window.confirm("¿Estás seguro de que deseas eliminar esta tarea?")) {
-                        deleteTask(task.id);
-                      }
-                    }}>
-                    Eliminar
-                  </button>
-                </TableCell>
-
-
-
           </TableRow>
           ))}
           
